0035827dea0f15af156369caf249c3b89219c490
Andrej Mihaliak <andrej.mihaliak@gmail.com>
2017-01-06 21:31:16 +0100

    added limit property

--- src/laravel-vue-pagination.js
+++ src/laravel-vue-pagination.js
@@ -15,22 +15,44 @@ module.exports = {
 					total: 0,
 				}
 			}
+		},
+		limit: {
+			type: Number,
+			default: 0
 		}
 	},
 
-	template: '<ul class="pagination" v-if="data.total > data.per_page">' +
-	'	<li v-if="data.prev_page_url">' +
-	'		<a href="#" aria-label="Previous" @click.prevent="selectPage(--data.current_page)"><span aria-hidden="true">&laquo;</span></a>' +
-	'	</li>' +
-	'	<li v-for="n in data.last_page" :class="{ \'active\': n == data.current_page }"><a href="#" @click.prevent="selectPage(n)">{{ n }}</a></li>' +
-	'	<li v-if="data.next_page_url">' +
-	'		<a href="#" aria-label="Next" @click.prevent="selectPage(++data.current_page)"><span aria-hidden="true">&raquo;</span></a>' +
-	'	</li>' +
-	'</ul>',
+	template: `
+	<ul class="pagination" v-if="data.total > data.per_page">
+		<li v-if="data.prev_page_url">
+			<a href="#" aria-label="Previous" @click.prevent="selectPage(--data.current_page)"><span aria-hidden="true">&laquo;</span></a>
+		</li>
+		<li v-for="n in getPages()" :class="{ 'active': n == data.current_page }"><a href="#" @click.prevent="selectPage(n)">{{ n }}</a></li>
+		<li v-if="data.next_page_url">
+			<a href="#" aria-label="Next" @click.prevent="selectPage(++data.current_page)"><span aria-hidden="true">&raquo;</span></a>
+		</li>
+	</ul>`,
 
 	methods: {
 		selectPage(page) {
 			this.$emit('pagination-change-page', page);
+		},
+		getPages() {
+			if (this.limit === -1) {
+				return 0;
+			}
+			
+			if (this.limit === 0) {
+				return this.data.last_page;
+			}
+
+        	let start = this.data.current_page - this.limit,
+        	    end   = this.data.current_page + this.limit + 1;
+
+        	start = start < 1 ? 1 : start;
+        	end   = end >= this.data.last_page ? this.data.last_page + 1 : end;
+          
+        	return new Array(end - start).fill().map((_,k) => k + start);
 		}
 	}
 };
--- test/laravel-vue-pagination.spec.js
+++ test/laravel-vue-pagination.spec.js
@@ -14,14 +14,20 @@ var exampleData = {
 		{ id: 3 },
 		{ id: 4 },
 		{ id: 5 },
+		{ id: 6 },
+		{ id: 7 },
+		{ id: 8 },
+		{ id: 9 },
+		{ id: 10 },
+		{ id: 11 },
 	],
 	from: 1,
-	last_page: 3,
+	last_page: 6,
 	next_page_url: 'http://example.com/page/2',
 	per_page: 2,
 	prev_page_url: null,
 	to: 3,
-	total: 5,
+	total: 11,
 };
 
 describe('LaravelVuePagination', function() {
@@ -31,20 +37,35 @@ describe('LaravelVuePagination', function() {
 		});
 
 		expect(vm.$el.nodeName).toBe('UL');
-		expect(vm.$el.getElementsByTagName('li').length).toBe(4);
+		expect(vm.$el.getElementsByTagName('li').length).toBe(7);
 		expect(vm.$el.getElementsByTagName('li')[0].classList).toContain('active');
+	});
+
+	it('has correct DOM structure with 1 link limit on page 3', function() {
+		exampleData.current_page = 3;
+		exampleData.next_page_url = 'http://example.com/page/4';
+		exampleData.prev_page_url = 'http://example.com/page/2';
+
+		const vm = getComponent(LaravelVuePagination, {
+			data: exampleData,
+			limit: 1
+		});
+
+		expect(vm.$el.nodeName).toBe('UL');
+		expect(vm.$el.getElementsByTagName('li').length).toBe(5);
+		expect(vm.$el.getElementsByTagName('li')[2].classList).toContain('active');
 	});
 
 	it('has correct DOM structure when on page 2', function() {
 		exampleData.current_page = 2;
-		exampleData.next_page_url = 'http://example.com/page/1';
-		exampleData.prev_page_url = 'http://example.com/page/3';
+		exampleData.next_page_url = 'http://example.com/page/3';
+		exampleData.prev_page_url = 'http://example.com/page/1';
 
 		const vm = getComponent(LaravelVuePagination, {
 			data: exampleData
 		});
 
-		expect(vm.$el.getElementsByTagName('li').length).toBe(5);
+		expect(vm.$el.getElementsByTagName('li').length).toBe(8);
 		expect(vm.$el.getElementsByTagName('li')[2].classList).toContain('active');
 	});
 
