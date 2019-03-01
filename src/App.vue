4ae5f2bb91725df1b5f1d25775137c91ff42b396
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-03-01 15:52:12 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,74 @@
 <template>
-    <div class="p-5">
-        <h1 class="mb-5">Laravel Vue Pagination Demo</h1>
-        <p>
-            <label for="limit">Limit</label><br>
-            <input type="number" id="limit" v-model="limit">
-        </p>
-        <p>
-            <label for="show-disabled">Show Disabled</label><br>
-            <input type="checkbox" id="show-disabled" v-model="showDisabled">
-        </p>
-        <p>
-            <label for="size">Size</label><br>
-            <select id="size" v-model="size">
-                <option value="small">Small</option>
-                <option value="default">Default</option>
-                <option value="large">Large</option>
-            </select>
-        </p>
-        <p class="mb-5">
-            <label for="align">Align</label><br>
-            <select id="align" v-model="align">
-                <option value="left">Left</option>
-                <option value="center">Center</option>
-                <option value="right">Right</option>
-            </select>
-        </p>
+    <div class="container mt-5">
+        <h1 class="mb-3">Laravel Vue Pagination Demo</h1>
+        <p class="mb-5">A Vue.js pagination component for Laravel paginators that works with Bootstrap.</p>
 
-        <p>Paginator Response:</p>
-        <pagination
-            :data="laravelData"
-            @pagination-change-page="getResults"
-            :limit="limit"
-            :show-disabled="showDisabled"
-            :size="size"
-            :align="align" />
+        <form class="mb-5" @submit.prevent>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="limit">Limit</label><br>
+                    <input type="number" id="limit" class="form-control" v-model="limit">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="size">Size</label><br>
+                    <select id="size" class="form-control" v-model="size">
+                        <option value="small">Small</option>
+                        <option value="default">Default</option>
+                        <option value="large">Large</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="align">Align</label><br>
+                    <select id="align" class="form-control" v-model="align">
+                        <option value="left">Left</option>
+                        <option value="center">Center</option>
+                        <option value="right">Right</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="show-disabled" v-model="showDisabled">
+                        <label class="form-check-label" for="show-disabled">Show Disabled</label>
+                    </div>
+                </div>
+            </div>
+        </form>
 
-        <p>API Resource Response:</p>
-        <pagination
-            :data="laravelResourceData"
-            @pagination-change-page="getResourceResults"
-            :limit="limit"
-            :show-disabled="showDisabled"
-            :size="size"
-            :align="align" />
+        <div class="card bg-light">
+            <div class="card-body p-5">
+                <div class="overflow-auto">
+                    <pagination
+                        class="mb-0"
+                        :data="laravelData"
+                        @pagination-change-page="getResults"
+                        :limit="limit"
+                        :show-disabled="showDisabled"
+                        :size="size"
+                        :align="align" />
+
+                    <!--pagination
+                        :data="laravelResourceData"
+                        @pagination-change-page="getResourceResults"
+                        :limit="limit"
+                        :show-disabled="showDisabled"
+                        :size="size"
+                        :align="align" /-->
+                </div>
+            </div>
+        </div>
+
+        <footer class="my-5 text-muted small">
+            <div class="row">
+                <div class="col-md-9">
+                    <p>Laravel Vue Pagination was created by <a href="https://gilbitron.me">Gilbert Pellegrom</a> from <a href="https://dev7studios.co">Dev7studios</a>. Released under the MIT license.</p>
+                </div>
+                <div class="col-md-3 text-right">
+                    <p><a href="https://github.com/gilbitron/laravel-vue-pagination">GitHub</a></p>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -78,7 +104,7 @@ export default {
         return {
             laravelData: {},
             laravelResourceData: {},
-            limit: 0,
+            limit: 2,
             showDisabled: false,
             size: 'default',
             align: 'left'
@@ -86,8 +112,8 @@ export default {
     },
 
     mounted () {
-        this.getResults();
-        this.getResourceResults();
+        this.getResults(10);
+        // this.getResourceResults();
     },
 
     methods: {
