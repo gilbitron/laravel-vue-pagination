ab66b8b9f89c66caa98cade16c263dcf13952230
Lindomar Silva <lindomar1016@hotmail.com>
2021-12-07 14:29:13 -0300

    Current Nav Slot

--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -29,7 +29,9 @@
             <li class="page-item pagination-page-nav" v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
                 <a class="page-link" href="#" v-on="pageButtonEvents(page)">
                     {{ page }}
-                    <span class="sr-only" v-if="page == computed.currentPage">(current)</span>
+                    <slot name="current-nav">
+                        <span class="sr-only" v-if="page == computed.currentPage">(current)</span>
+                    </slot>
                 </a>
             </li>
 
