d9dff3f1bdc21431b9fd7d7b95fd345900f6d201
Gilbert Pellegrom <gilbert@pellegrom.me>
2022-02-18 13:55:57 +0000

    Configure build:app

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,37 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/LaravelVuePagination.vue'),
-            name: 'LaravelVuePagination',
-            fileName: (format) => `laravel-vue-pagination.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue',
+export default defineConfig(({ mode }) => {
+    if (mode === 'app') {
+        return {
+            plugins: [vue()],
+        };
+    } else {
+        return {
+            plugins: [vue()],
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url)),
                 },
             },
-        },
-    },
+            build: {
+                lib: {
+                    entry: path.resolve(
+                        __dirname,
+                        'src/LaravelVuePagination.vue'
+                    ),
+                    name: 'LaravelVuePagination',
+                    fileName: (format) => `laravel-vue-pagination.${format}.js`,
+                },
+                rollupOptions: {
+                    external: ['vue'],
+                    output: {
+                        globals: {
+                            vue: 'Vue',
+                        },
+                    },
+                },
+            },
+        };
+    }
 });
