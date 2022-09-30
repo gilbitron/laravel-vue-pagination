b439b424b78919f70ef96c7646952f8b0975dd4a
Gilbert Pellegrom <gilbert@pellegrom.me>
2022-09-30 21:23:41 +0100

    Add Bootstrap5Pagination.vue

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,9 @@
 import RenderlessPagination from './RenderlessPagination.vue';
 import Bootstrap4Pagination from './Bootstrap4Pagination.vue';
+import Bootstrap5Pagination from './Bootstrap5Pagination.vue';
 
 export { 
     RenderlessPagination, 
     Bootstrap4Pagination, 
+    Bootstrap5Pagination,
 };
